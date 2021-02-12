b98c744550204aebbba6a9190c98bbcc23c718ad
Jake Cornwall Scoones <jake.cornwallscoones@crick.ac.uk>
2021-02-12 15:54:08 +0000

    In silico eCRE analysis

--- main.sh
+++ main.sh
@@ -4,16 +4,16 @@
 #################
 #Set up environment
 ##################
-cd setup
-./setup.sh
-cd .. 
+# cd setup
+# ./setup.sh
+# cd .. 
 
 ##################
 #Make lookup table
 ###################
-cd reference
-./get_gene_info.sh
-cd .. 
+# cd reference
+# ./get_gene_info.sh
+# cd .. 
 eval "$(conda shell.bash hook)"
 source activate synCRE
 python lookup_table/make_alias_dict.py
