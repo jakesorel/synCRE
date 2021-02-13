8374657b2888730318c394106e065c3df6cc92a4
Jake Cornwall Scoones <jake.cornwallscoones@crick.ac.uk>
2021-02-13 19:18:48 +0000

    In silico eCRE analysis

--- pybed_test.py
+++ pybed_test.py
@@ -3,4 +3,4 @@ from pybedtools import BedTool
 eCRE = BedTool("reference/eCRE_locs/o2e33.bed")
 archetypes = BedTool("reference/archetypes/mm10.archetype_motifs.v1.0.chr16.bed")
 
-eCRE.intersect(archetypes,'-wb').saveas("reference/intersected.bed")
+eCRE.intersect(archetypes,wb=True).saveas("reference/intersected.bed")
