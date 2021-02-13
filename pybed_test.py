8dc6ae71c1760b7235be57b4ca5f16eee0ad8842
Jake Cornwall Scoones <jake.cornwallscoones@crick.ac.uk>
2021-02-13 19:26:57 +0000

    In silico eCRE analysis

--- pybed_test.py
+++ pybed_test.py
@@ -1,6 +1,18 @@
 from pybedtools import BedTool
+from reformat_functions.functions import *
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 
-eCRE = BedTool("reference/eCRE_locs/o2e33.bed")
-archetypes = BedTool("reference/archetypes/mm10.archetype_motifs.v1.0.chr16.bed")
-
-eCRE.intersect(archetypes,wb=True).saveas("reference/intersected.bed")
+make_directory("results/archetype_beds")
+eCRE_names = os.listdir("reference/eCRE_locs")
+for name in eCRE_names:
+    eCRE = BedTool("reference/eCRE_locs/%s"%name)
+    chromosome = eCRE.to_dataframe()["chrom"].values[0]
+    archetypes = BedTool("reference/archetypes/mm10.archetype_motifs.v1.0.%s.bed"%chromosome)
+    eCREname = name.split(".bed")[0]
+    make_directory("results/archetype_beds/%s"%eCREname)
+    make_directory("results/archetype_beds/%s/all_beds"%eCREname)
+    eCRE.intersect(archetypes,wb=True).saveas("results/archetype_beds/%s/all_beds/%s_archetypes.bed"%(eCREname,eCREname))
+    print("Ran intersect on %s"%name)
