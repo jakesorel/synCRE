30e7c08924bb5b935f630bc428f087b2ee76f694
Jake Cornwall Scoones <jake.cornwallscoones@crick.ac.uk>
2021-02-13 19:42:25 +0000

    In silico eCRE analysis

--- motif_finder/intersect_archetypes.py
+++ motif_finder/intersect_archetypes.py
@@ -20,7 +20,7 @@ for name in eCRE_names:
     archetype_intersect = eCRE.intersect(archetypes,wb=True).saveas("results/archetype_beds/%s/all_beds/%s_archetypes.bed"%(eCREname,eCREname))
     print("Ran intersect on %s"%name)
     df = archetype_intersect.to_dataframe()
-    out_df = pd.DataFrame(np.array([df[i] for i in range(3,9)]).T)
+    out_df = pd.DataFrame(df[df.columns[3:9]])
     out_df.to_csv("results/archetype_beds/%s/all_beds/%s_archetypes_clean.bed"%(eCREname,eCREname),sep="\t",index=False,header=False)
     print("Saved clean version")
 
