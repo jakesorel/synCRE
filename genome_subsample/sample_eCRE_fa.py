28b116c15559b112b25c293833c461097cd420d7
Jake Cornwall Scoones <jake.cornwallscoones@crick.ac.uk>
2021-02-16 14:17:07 +0000

    In silico eCRE analysis

--- genome_subsample/sample_eCRE_fa.py
+++ genome_subsample/sample_eCRE_fa.py
@@ -7,6 +7,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 from reformat_functions.functions import *
 
+
 make_directory("results/fasta")
 make_directory("results/fasta/scrap")
 make_directory("results/fasta/by_eCRE")
