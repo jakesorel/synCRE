270e33ba79cf37e464fcd3d23a0ccd8c2e0a17b1
Jake Cornwall Scoones <jake.cornwallscoones@crick.ac.uk>
2021-02-16 14:34:53 +0000

    In silico eCRE analysis

--- genome_subsample/sample_eCRE_fa.py
+++ genome_subsample/sample_eCRE_fa.py
@@ -1,3 +1,21 @@
+# from pybedtools import BedTool,example_filename
+# import os, sys
+# import pandas as pd
+# import numpy as np
+# currentdir = os.path.dirname(os.path.realpath(__file__))
+# parentdir = os.path.dirname(currentdir)
+# sys.path.append(parentdir)
+# from reformat_functions.functions import *
+#
+# #
+# # chr16fa = "/camp/lab/luscomben/home/shared/ref/genomes/mouse/gencode_GRCm38/chr_sep_files/chr16.fa.gz"
+# # chr16fa = "chr16.fa.gz"
+# # unzip(chr16fa,"out.fa")
+# # fa_file = example_filename('%s/out.fa'%currentdir)
+# # olig2fa = BedTool("reference/eCRE_locs/Olig2.bed").sequence("out.fa",fo="sample.fa")
+# # print(open(olig2fa.seqfn).read())
+# #
+
 from pybedtools import BedTool,example_filename
 import os, sys
 import pandas as pd
@@ -19,8 +37,10 @@ for bed_file in bed_files:
     chrfa = "/camp/lab/luscomben/home/shared/ref/genomes/mouse/gencode_GRCm38/chr_sep_files/%s.fa.gz"%chrom
     # chr16fa = "chr16.fa.gz"
     unzip(chrfa,"results/fasta/scrap/out.fa")
-    olig2fa = eCRE.sequence("results/fasta/scrap/out.fa",fo="results/fasta/by_eCRE/%s.fa"%eCRE_name)
+    try:
+        eCRE.sequence("results/fasta/scrap/out.fa",fo="results/fasta/by_eCRE/%s.fa"%eCRE_name)
+    except RuntimeWarning:
+        pass
     os.remove("results/fasta/scrap/out.fa")
     os.remove("results/fasta/scrap/out.fai")
     print(eCRE_name," complete")
-
