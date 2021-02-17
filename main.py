9310ce8d72f9e724118effb232387cafd1039e73
Jake Cornwall Scoones <jake.cornwallscoones@crick.ac.uk>
2021-02-17 11:08:42 +0000

    In silico eCRE analysis

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from synCRE import *
 # expr = Expression(RNA_seq_file="reference/RNA_seq_local.txt")
 # expr.run_all()
 
-mtf = Motif_Finder()
+# mtf = Motif_Finder()
 # mtf.make_pmf()
 # mtf.sample_eCRE_sequence()
 # mtf.find_motifs([0.001])
-mtf.motif_to_bed()
-mtf.motifs_by_archetype()
-mtf.motifs_by_cluster()
+# mtf.motif_to_bed()
+# mtf.motifs_by_archetype()
+# mtf.motifs_by_cluster()
 
 print("motifs found")
 
