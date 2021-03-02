fbd60b01d58b40429c538e2841248b991ecc4306
Jake Cornwall Scoones <jake.cornwallscoones@crick.ac.uk>
2021-03-02 12:16:48 +0000

    In silico eCRE analysis

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 from synCRE import *
-##############################
-# Make the lookup table
-##############################
-# lkp = Lookup(RNA_seq_file="reference/RNA_seq.txt")
-
-##############################
-# Cluster RNA expression
-##############################
-# expr = Expression(RNA_seq_file="reference/RNA_seq_local.txt")
-# expr.run_all()
-
-##############################
-# Find motifs within each eCRE
-##############################
-mtf = Motif_Finder()
-# mtf.make_pmf()
-# mtf.sample_eCRE_sequence()
-# mtf.find_motifs(0.001)
-
-required_dicts = {"Olig2": {"Olig2": 1, "Nkx2-2": 1, "Gli3": 1},
-                  "Nkx2-2": {"Olig2": 1, "Nkx2-2": 1, "Gli3": 1},
-                  "Pax6": {"Olig2": 1, "Nkx2-2": 1}}
-mtf.filter_motifs_by_hit(required_dicts)
-mtf.plot_motif_distributions()
-mtf.motif_to_bed()
-# # mtf.motifs_to_bedgraph()
-mtf.motifs_by_archetype(collapse=True)
-mtf.motifs_by_cluster(make_bedgraph=False)
-mtf.collapse_all_bed()
-mtf.collapse_bed_relevant_clusters(clusters=[0,3,4,5])
-print("motifs found")
+# ##############################
+# # Make the lookup table
+# ##############################
+# # lkp = Lookup(RNA_seq_file="reference/RNA_seq.txt")
+#
+# ##############################
+# # Cluster RNA expression
+# ##############################
+# # expr = Expression(RNA_seq_file="reference/RNA_seq_local.txt")
+# # expr.run_all()
+#
+# ##############################
+# # Find motifs within each eCRE
+# ##############################
+# mtf = Motif_Finder()
+# # mtf.make_pmf()
+# # mtf.sample_eCRE_sequence()
+# # mtf.find_motifs(0.001)
+#
+# required_dicts = {"Olig2": {"Olig2": 1, "Nkx2-2": 1, "Gli3": 1},
+#                   "Nkx2-2": {"Olig2": 1, "Nkx2-2": 1, "Gli3": 1},
+#                   "Pax6": {"Olig2": 1, "Nkx2-2": 1}}
+# mtf.filter_motifs_by_hit(required_dicts)
+# mtf.plot_motif_distributions()
+# mtf.motif_to_bed()
+# # # mtf.motifs_to_bedgraph()
+# mtf.motifs_by_archetype(collapse=True)
+# mtf.motifs_by_cluster(make_bedgraph=False)
+# mtf.collapse_all_bed()
+# mtf.collapse_bed_relevant_clusters(clusters=[0,3,4,5])
+# print("motifs found")
 #
 ##############################
 # Plot data
@@ -42,10 +42,10 @@ for eCRE in eCREs:
         eCRE = eCRE.split(".bed")[0]
         plot = GenomePlot(eCRE,plot_constructs=True,plot_bw=True,plot_genes=True,plot_phylo=True)
         plot.ini_all_motifs()
-        plot.ini_by_cluster()
-        plot.ini_relevant_clusters()
-        plot.ini_by_cluster_merge()
-        plot.ini_by_candidate()
+        # plot.ini_by_cluster()
+        # plot.ini_relevant_clusters()
+        # plot.ini_by_cluster_merge()
+        # plot.ini_by_candidate()
         plot.make_plots(parallel=True,suppress=True)
         print("""
         ################################################
