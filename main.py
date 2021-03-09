685417afc2f0ae597c012403b83c7b348657a26d
Jake Cornwall Scoones <jake.cornwallscoones@crick.ac.uk>
2021-03-09 12:25:17 +0000

    In silico eCRE analysis

--- main.py
+++ main.py
@@ -13,24 +13,24 @@ from synCRE import *
 # ##############################
 # # Find motifs within each eCRE
 # ##############################
-# mtf = Motif_Finder()
+mtf = Motif_Finder()
 # mtf.make_pmf()
 # mtf.sample_eCRE_sequence()
 # mtf.find_motifs(4)
 # mtf.make_truth_matrix()
 #
-# required_dicts = {"Olig2": {"Olig2": 1, "Nkx2-2": 1, "Gli3": 1},
-#                   "Nkx2-2": {"Olig2": 1, "Gli3": 1},
-#                   "Pax6": {"Olig2": 1, "Nkx2-2": 1}}
-# mtf.filter_motifs_by_hit(required_dicts)
-# mtf.plot_motif_distributions()
-# mtf.motif_to_bed()
-# # # mtf.motifs_to_bedgraph()
-# mtf.motifs_by_archetype(collapse=True)
-# mtf.motifs_by_cluster(make_bedgraph=False)
-# mtf.collapse_all_bed()
-# mtf.collapse_bed_relevant_clusters(clusters=[0,3,4,5])
-# print("motifs found")
+required_dicts = {"Olig2": {"Olig2": 1, "Nkx2-2": 1, "Gli3": 1},
+                  "Nkx2-2": {"Olig2": 1, "Gli3": 1},
+                  "Pax6": {"Olig2": 1, "Nkx2-2": 1}}
+mtf.filter_motifs_by_hit(required_dicts)
+mtf.plot_motif_distributions()
+mtf.motif_to_bed()
+# # mtf.motifs_to_bedgraph()
+mtf.motifs_by_archetype(collapse=True)
+mtf.motifs_by_cluster(make_bedgraph=False)
+mtf.collapse_all_bed()
+mtf.collapse_bed_relevant_clusters(clusters=[0,3,4,5])
+print("motifs found")
 #
 ##############################
 # Plot data
@@ -44,7 +44,7 @@ for eCRE in eCREs:
         plot = GenomePlot(eCRE,plot_constructs=True,plot_bw=True,plot_genes=False,plot_phylo=False)
         plot.ini_all_motifs()
         # plot.ini_by_cluster()
-        # plot.ini_relevant_clusters()
+        plot.ini_relevant_clusters()
         # plot.ini_by_cluster_merge()
         # plot.ini_by_candidate()
         plot.make_plots(parallel=True,suppress=True)
