4ba51bb5ed53b33fabfec249a71c21416e1949ea
Jake <jake.cornwallscoones@crick.ac.uk>
2021-05-20 15:52:24 +0100

    In silico eCRE analysis

--- synCRE.py
+++ synCRE.py
@@ -816,7 +816,7 @@ sort -k2,2n -k3,3n results/motifs/bed/%s.bed -o results/motifs/bed/%s.bed
                            index=None)
 
                 ##make a merged bed file lacking the colours.
-                merge_bed_file_name = "results/motifs/relevant_clusters/%s" % (bed.split(".bed")+"_merge.bed")
+                merge_bed_file_name = "results/motifs/relevant_clusters/%s" % (bed.split(".bed")[0]+"_merge.bed")
                 adf[adf.columns[:3]].to_csv(merge_bed_file_name, sep="\t", header=None,
                            index=None)
                 BedTool(merge_bed_file_name).merge().saveas(merge_bed_file_name)
