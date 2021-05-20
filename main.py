c1fe7cc41dd26bc0ace531b83682e7a5619629bd
Jake <jake.cornwallscoones@crick.ac.uk>
2021-05-20 11:29:28 +0100

    In silico eCRE analysis

--- main.py
+++ main.py
@@ -43,14 +43,14 @@ max_val_dict={"Olig2":25,"Nkx2-2":20,"Pax6":20}
 for eCRE in eCREs:
     if ".bed" in eCRE:
         eCRE = eCRE.split(".bed")[0]
-        plot = GenomePlot(eCRE,plot_constructs=True,plot_bw=True,plot_genes=False,plot_phylo=False)
+        plot = GenomePlot(eCRE,plot_constructs=False,plot_bw=True,plot_genes=False,plot_phylo=False)
         # plot.ini_all_motifs()
         plot.ini_atac(max_value=max_val_dict[eCRE])
         # plot.ini_by_cluster()
         # plot.ini_relevant_clusters()
         # plot.ini_by_cluster_merge()
         # plot.ini_by_candidate()
-        plot.make_plots(parallel=True,suppress=True,width=40)
+        plot.make_plots(parallel=True,suppress=True,width=25)
         print("""
         ################################################
         Plots for %s complete
